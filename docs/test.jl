a0ba77da47accbc04520a355277a06ff937d733c
Kapil Panwar <panwar.kapil91@gmail.com>
2023-03-20 22:38:01 +0530

    notebook.jl

---
+++ docs/test.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 3c8c61ec-c73c-11ed-2efe-9fb5e733861b
+md"# Welcome to Julia for Data Science "
+
+# ╔═╡ Cell order:
+# ╠═3c8c61ec-c73c-11ed-2efe-9fb5e733861b
